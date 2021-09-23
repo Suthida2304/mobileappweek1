9e839bc0048d6fbe25d5e805d324846168fd8ee2
Suthida2304 <s6302041510125@email.kmutnb.ac.th>
2021-09-23 16:55:33 +0700

    การจัดการข้อมูลแบบ Local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:mobileappweek1/config/constant.dart';
+import 'package:mobileappweek1/screen/dashboard.dart';
 import 'package:mobileappweek1/screen/index.dart';
 import 'package:mobileappweek1/screen/login.dart';
 import 'package:mobileappweek1/screen/register.dart';
 
 import 'screen/index.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Index(),
       routes: {
-        'Login' : (context) => Login(),
+        'Login': (context) => Login(),
         'Register': (context) => Register(),
+        'Dashboard': (context) => Dashboard(),
       },
       theme: ThemeData(
         primaryColor: pColor,
